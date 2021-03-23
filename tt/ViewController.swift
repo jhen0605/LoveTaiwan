3af001b3a5e929c37e4990f7c1240c774937854b
jhen0605 <amy95173@gmail.com>
2021-03-24 05:53:16 +0800

    Initial Commit

---
+++ tt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tt
+//
+//  Created by 簡吟真 on 2021/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
